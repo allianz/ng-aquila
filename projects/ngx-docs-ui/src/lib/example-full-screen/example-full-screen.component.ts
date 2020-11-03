631023093c9601f922142afe08419c26a4c323d5
extern.milde_silvia@allianz.com <silvia.milde@sinnerschrader.com>
2020-11-03 13:14:46 +0100

    chore(fullscreen-example-view): add 'back' button (#87)

--- projects/ngx-docs-ui/src/lib/example-full-screen/example-full-screen.component.ts
+++ projects/ngx-docs-ui/src/lib/example-full-screen/example-full-screen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ThemeSwitcherService } from '../documentation/theme-switcher/theme-switcher.service';
+import { Location } from '@angular/common';
 
 @Component({
   templateUrl: './example-full-screen.component.html',
@@ -10,8 +11,8 @@ export class ExampleFullScreenComponent implements OnInit {
 
   constructor(
     private _route: ActivatedRoute,
-    private _themeSwitcherService: ThemeSwitcherService
-    ) {}
+    private _themeSwitcherService: ThemeSwitcherService,
+    public _location: Location) {}
 
   ngOnInit() {
     const themeName = this._route.snapshot.queryParamMap.get('theme');
--- projects/ngx-docs-ui/src/lib/example-full-screen/example-full-screen.module.ts
+++ projects/ngx-docs-ui/src/lib/example-full-screen/example-full-screen.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ExampleFullScreenComponent } from './example-full-screen.component';
 import { RouterModule } from '@angular/router';
+import { NxButtonModule } from '@aposin/ng-aquila/button';
 
 @NgModule({
   imports: [
     RouterModule,
+    NxButtonModule
   ],
   exports: [ExampleFullScreenComponent],
   declarations: [ExampleFullScreenComponent],
