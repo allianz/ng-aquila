9bb0029de0b1ab93e3abc5624e269f255a0da9a1
extern.fahrenschon_philipp@allianz.com <philipp.fahrenschon@sinnerschrader.com>
2021-01-13 15:25:24 +0100

    chore: fix precommit linting path (#183)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,7 @@ function fileList(files) {
 }
 
 module.exports = {
-  'projects/ng-aquila/(!documentation)**/*.ts': (files) => `ng lint ng-aquila --fix ${fileList(files)}`,
+  'projects/ng-aquila/!(documentation)/**/*.ts': (files) => `ng lint ng-aquila --fix ${fileList(files)}`,
   'projects/ngx-docs-ui/**/*.ts': (files) => `ng lint ngx-docs-ui --fix ${fileList(files)}`,
   'projects/opensource-documentation/**/*.ts': (files) => `ng lint opensource-documentation --fix ${fileList(files)}`,
   '*.scss': 'npm run lint:scss'
