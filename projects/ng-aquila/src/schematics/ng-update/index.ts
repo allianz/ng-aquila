a9406c68f002c0f41adbeb0962b5d6f0f9f5bd67
extern.fahrenschon_philipp@allianz.com <philipp.fahrenschon@sinnerschrader.com>
2021-01-29 15:16:22 +0100

    chore(ng-update): show margin removal warning (#213)

--- projects/ng-aquila/src/schematics/ng-update/index.ts
+++ projects/ng-aquila/src/schematics/ng-update/index.ts
@@ -1,5 +1,5 @@
 import { MiscTemplateMigration } from './migrations/misc-template';
-import {Rule, SchematicContext} from '@angular-devkit/schematics';
+import { Rule, SchematicContext } from '@angular-devkit/schematics';
 import {
   createMigrationSchematicRule,
   NullableDevkitMigration,
@@ -37,19 +37,35 @@ const customMigrations = [
 
 /** Entry point for the migration schematics with target of Angular CDK 12.0.0 */
 export function updateToV11(): Rule {
-  return createMigrationSchematicRule(TargetVersion.V11, customMigrations, upgradeData, onMigrationComplete);
+  return createMigrationSchematicRule(TargetVersion.V11, customMigrations, upgradeData, v11MarginsWarning);
 }
 
 /** Function that will be called when the migration completed. */
 function onMigrationComplete(context: SchematicContext, targetVersion: TargetVersion,
-                             hasFailures: boolean) {
+  hasFailures: boolean) {
   context.logger.info('');
   context.logger.info(`  ✓  Updated @aposin/ng-aquila to ${targetVersion}`);
   context.logger.info('');
 
   if (hasFailures) {
     context.logger.warn(
-        '  ⚠  Some issues were detected but could not be fixed automatically. Please check the ' +
-        'output above and fix these issues manually.');
+      '  ⚠  Some issues were detected but could not be fixed automatically. Please check the ' +
+      'output above and fix these issues manually.');
   }
+}
+
+function v11MarginsWarning(context: SchematicContext, targetVersion: TargetVersion,
+  hasFailures: boolean) {
+
+  // call the generic complete function for all schematics
+  onMigrationComplete(context, targetVersion, hasFailures);
+
+  // this major we want to throw this additional warning
+  context.logger.warn(
+    `
+    ⚠ In ng-aquila v11 some components got their default outer margins removed please check your applications for visual changes.
+    You can find more details at:
+    https://aposin.github.io/ng-aquila/guides/releases
+    `
+  );
 }
