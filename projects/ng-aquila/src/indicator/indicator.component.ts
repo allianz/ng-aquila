61ad2820543beb5528950551be85d1dd39f459e3
extern.kostyunin_gleb@allianz.de <gleb.kostyunin@sinnerschrader.com>
2021-03-11 12:20:28 +0100

    feat(indicator): positioning enhancements (#249)

--- projects/ng-aquila/src/indicator/indicator.component.ts
+++ projects/ng-aquila/src/indicator/indicator.component.ts
@@ -1,10 +1,35 @@
 
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
+export type NxIndicatorPosition = 'over-text' | 'over-icon' | 'after-text' | 'with-overlap';
 @Component({
   selector: 'nx-indicator',
   template: '<ng-content></ng-content>',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrls: ['./indicator.component.scss']
+  styleUrls: ['./indicator.component.scss'],
+  host: {
+    '[class.nx-indicator]': 'true',
+    '[class.nx-indicator--over-text]': 'this._hasPosition("over-text")',
+    '[class.nx-indicator--over-icon]': 'this._hasPosition("over-icon")',
+    '[class.nx-indicator--after-text]': 'this._hasPosition("after-text")',
+    '[class.nx-indicator--with-overlap]': 'this._hasPosition("with-overlap")',
+  }
 })
-export class NxIndicatorComponent { }
+export class NxIndicatorComponent {
+  private _position: NxIndicatorPosition[] = [];
+
+  /** Sets the indicator positioning preset.
+   * Should be one or more of 'over-text', 'over-icon', 'after-text', 'with-overlap'
+  */
+  @Input()
+  set position(value: string) {
+    this._position = value.split(' ') as NxIndicatorPosition[];
+  }
+  get position(): string {
+    return this._position.join(' ');
+  }
+
+  _hasPosition(position: NxIndicatorPosition) {
+    return this._position.indexOf(position) > -1;
+  }
+}
