8aa2e592994b5bad63f9f2a90d567b5c24322b37
Fabian Maschewski <fabian.maschewski@kaiser-x.com>
2021-11-04 16:50:08 +0100

    chore: update tslint scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  'projects/ng-aquila/!(documentation)/**/!(*.spec).ts': (files) => files.map(file => `tslint -c projects/ng-aquila/tslint.json -p projects/ng-aquila/tsconfig.lib.json -e **/node_modules/** --fix ${file}`),
-  'projects/ng-aquila/!(documentation)/!(schematics)/*.spec.ts': (files) => files.map(file => `tslint -c projects/ng-aquila/tslint.json -p projects/ng-aquila/tsconfig.spec.json -e **/node_modules/** --fix ${file}`),
-  'projects/ngx-docs-ui/**/!(*.spec).ts': (files) => files.map(file => `tslint -c projects/ngx-docs-ui/tslint.json -p projects/ngx-docs-ui/tsconfig.lib.json -e **/node_modules/** --fix ${file}`),
-  'projects/ngx-docs-ui/**/*.spec.ts': (files) => files.map(file => `tslint -c projects/ngx-docs-ui/tslint.json -p projects/ngx-docs-ui/tsconfig.spec.json -e **/node_modules/** --fix ${file}`),
-  'projects/opensource-documentation/**/*.ts': (files) => files.map(file => `tslint -c projects/opensource-documentation/tslint.json opensource-documentation -p projects/opensource-documentation/tsconfig.app.json -e **/node_modules/** --fix ${file}`),
+  'projects/ng-aquila/!(documentation)/**/!(*.spec).ts': (files) => files.map(file => `tslint -c projects/ng-aquila/tslint.json -p projects/ng-aquila/tsconfig.lib.json --fix ${file}`),
+  'projects/ng-aquila/!(documentation)/!(schematics)/*.spec.ts': (files) => files.map(file => `tslint -c projects/ng-aquila/tslint.json -p projects/ng-aquila/tsconfig.spec.json --fix ${file}`),
+  'projects/ngx-docs-ui/**/!(*.spec).ts': (files) => files.map(file => `tslint -c projects/ngx-docs-ui/tslint.json -p projects/ngx-docs-ui/tsconfig.lib.json --fix ${file}`),
+  'projects/ngx-docs-ui/**/*.spec.ts': (files) => files.map(file => `tslint -c projects/ngx-docs-ui/tslint.json -p projects/ngx-docs-ui/tsconfig.spec.json --fix ${file}`),
+  'projects/opensource-documentation/**/*.ts': (files) => files.map(file => `tslint -c projects/opensource-documentation/tslint.json opensource-documentation -p projects/opensource-documentation/tsconfig.app.json --fix ${file}`),
   '*.scss': 'npm run lint:scss'
 }
