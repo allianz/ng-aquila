0af334296a6ec102158018af981a725055a03321
Igor Milly <extern.milly_igor@allianz.com>
2022-01-19 10:10:54 +0100

    chore: misc doc project cleanup (#480)

--- projects/opensource-documentation/src/app/app.component.ts
+++ projects/opensource-documentation/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NxvNavigationService } from '@aposin/ngx-docs-ui';
 
 @Component({
     selector: 'doc-root',
-    templateUrl: './app.component.html',
+    template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-    title = 'documentation';
+export class AppComponent implements OnInit {
+    constructor(private _navigationService: NxvNavigationService) {}
 
-    constructor(private _navigationService: NxvNavigationService) {
+    ngOnInit() {
         this._navigationService.resetScrollPositionWatcher();
     }
 }
