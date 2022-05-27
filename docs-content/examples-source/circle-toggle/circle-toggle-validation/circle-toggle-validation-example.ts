7ffada323153b457e2d2480f43f9445ad7cf8b86
yd-allianz <yd-allianz@users.noreply.github.com>
2022-05-27 11:29:01 +0000

    Deploying to gh-pages from  @ 3864646c72c2281fa0c153051f0eec436bec6c02 🚀

---
+++ docs-content/examples-source/circle-toggle/circle-toggle-validation/circle-toggle-validation-example.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ValidatorFn,
+    Validators,
+} from '@angular/forms';
+
+const selectBothValidator: ValidatorFn = (ctrl: AbstractControl) => {
+    const heart = ctrl.get('heart')?.value;
+    const car = ctrl.get('car')?.value;
+    const isSelectBoth = heart && car;
+    return isSelectBoth ? null : { selectBoth: heart && car };
+};
+
+/**
+ * @title Validation example
+ */
+@Component({
+    selector: 'circle-toggle-validation-example',
+    templateUrl: './circle-toggle-validation-example.html',
+    styleUrls: ['./circle-toggle-validation-example.css'],
+})
+export class CircleToggleValidationExampleComponent {
+    formBuilder = new FormBuilder();
+
+    testForm = this.formBuilder.group({
+        reactiveToggle: ['', Validators.required],
+    });
+
+    testForm2 = this.formBuilder.group({
+        heart: [false, Validators.requiredTrue],
+    });
+
+    testForm3 = this.formBuilder.group(
+        {
+            heart: [false, Validators.requiredTrue],
+            car: [false, Validators.requiredTrue],
+        },
+        { validators: [selectBothValidator] },
+    );
+
+    single = false;
+
+    sampleValues = [
+        {
+            value: 'A',
+            icon: 'product-heart',
+            hint: 'Hint A',
+            label: 'Label A',
+            selected: false,
+        },
+        {
+            value: 'B',
+            icon: 'product-car',
+            hint: 'Hint B',
+            label: 'Label B',
+            selected: true,
+        },
+        {
+            value: 'C',
+            icon: 'product-plane',
+            hint: 'Hint C',
+            label: 'Label C',
+            selected: false,
+        },
+    ];
+
+    patch(value: string): void {
+        this.testForm.patchValue({ reactiveToggle: value });
+    }
+}
