a6ea03daaca72187830299cb6fc8c55d14adecf0
Igor Milly <extern.milly_igor@allianz.com>
2022-08-08 13:33:30 +0200

    docs: footer dynamic year (#678)

--- projects/opensource-documentation/src/app/footer/footer.component.ts
+++ projects/opensource-documentation/src/app/footer/footer.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+    readonly currentYear = new Date();
+}
