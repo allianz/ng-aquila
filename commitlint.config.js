62e86096b3af3bc5e43969334f127f3262ba8923
Igor Milly <dmkttzm@allianzms.onmicrosoft.com>
2022-09-15 14:00:36 +0200

    chore: adapt commit lint config (#728)

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-angular'],
-    rules: {
-        'subject-max-length': [2, 'always', 120],
-        'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'release', 'revert', 'style', 'test']],
-    },
-};
