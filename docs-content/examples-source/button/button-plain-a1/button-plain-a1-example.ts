6179ef8fb4d9bacc39a06c682d7b8b2021ef03ad
yd-allianz <yd-allianz@users.noreply.github.com>
2023-06-06 14:48:25 +0000

    Deploying to gh-pages from  @ 711c3f247e2d26e5bd5d9f1d544a73290ffad909 🚀

---
+++ docs-content/examples-source/button/button-plain-a1/button-plain-a1-example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Plain Button One Allianz additions
+ */
+@Component({
+    selector: 'button-plain-a1-example',
+    templateUrl: './button-plain-a1-example.html',
+    styleUrls: ['./button-plain-a1-example.css'],
+})
+export class ButtonPlainA1ExampleComponent {}
