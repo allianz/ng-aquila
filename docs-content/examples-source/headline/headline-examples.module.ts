789b7b86dd828335682cc8c758718ecad8b4ca03
yd-allianz <yd-allianz@users.noreply.github.com>
2023-06-13 15:17:32 +0000

    Deploying to gh-pages from  @ c9ac362f634ed6ae2f8763bf57000eb97525e66c 🚀

--- docs-content/examples-source/headline/headline-examples.module.ts
+++ docs-content/examples-source/headline/headline-examples.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { NxHeadlineModule } from '@aposin/ng-aquila/headline';
 import { NxLinkModule } from '@aposin/ng-aquila/link';
+import { NxTableModule } from '@aposin/ng-aquila/table';
 
 import { HeadlineFontWeightsExampleComponent } from './headline-font-weights/headline-font-weights-example';
 import { HeadlineLinksExampleComponent } from './headline-links/headline-links-example';
 import { HeadlineNegativeExampleComponent } from './headline-negative/headline-negative-example';
-import { HeadlineSizesExampleComponent } from './headline-sizes/headline-sizes-example';
+import { HeadlineSizesA1ExampleComponent } from './headline-sizes-a1/headline-sizes-a1-example';
+import { HeadlineSizesDeprecatedExampleComponent } from './headline-sizes-deprecated/headline-sizes-deprecated-example';
+import { HeadlineSizesMappingExampleComponent } from './headline-sizes-mapping/headline-sizes-mapping-example';
 
 const EXAMPLES = [
     HeadlineFontWeightsExampleComponent,
     HeadlineLinksExampleComponent,
     HeadlineNegativeExampleComponent,
-    HeadlineSizesExampleComponent,
+    HeadlineSizesDeprecatedExampleComponent,
+    HeadlineSizesA1ExampleComponent,
+    HeadlineSizesMappingExampleComponent,
 ];
 
 @NgModule({
-    imports: [NxHeadlineModule, NxLinkModule],
+    imports: [NxHeadlineModule, NxLinkModule, NxTableModule],
     declarations: [EXAMPLES],
     exports: [EXAMPLES],
 })
@@ -25,7 +30,10 @@ export class HeadlineExamplesModule {
             'headline-font-weights': HeadlineFontWeightsExampleComponent,
             'headline-links': HeadlineLinksExampleComponent,
             'headline-negative': HeadlineNegativeExampleComponent,
-            'headline-sizes': HeadlineSizesExampleComponent,
+            'headline-sizes-deprecated':
+                HeadlineSizesDeprecatedExampleComponent,
+            'headline-sizes-a1': HeadlineSizesA1ExampleComponent,
+            'headline-sizes-mapping': HeadlineSizesMappingExampleComponent,
         };
     }
 }
---
+++ docs-content/examples-source/headline/headline-sizes-deprecated/headline-sizes-deprecated-example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Deprecated Headline Sizes Example
+ */
+@Component({
+    selector: 'headline-sizes-deprecated-example',
+    templateUrl: './headline-sizes-deprecated-example.html',
+    styleUrls: ['./headline-sizes-deprecated-example.css'],
+})
+export class HeadlineSizesDeprecatedExampleComponent {}
