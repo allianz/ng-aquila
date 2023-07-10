2928f7aaaecc13a17fdaa21afdb5737038eaee53
extern.fahrenschon_philipp@allianz.com <philipp.fahrenschon@satellytes.com>
2023-07-10 09:21:31 +0200

    fix(datefield): allow NX_DATE_LOCALE to be provided in platformBrowserDynamic (#976)

--- projects/ng-aquila/src/datefield/adapter/index.ts
+++ projects/ng-aquila/src/datefield/adapter/index.ts
@@ -1,5 +1,5 @@
 import { PlatformModule } from '@angular/cdk/platform';
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule, Optional, SkipSelf } from '@angular/core';
 
 import { NxDateAdapter } from './date-adapter';
 import { NX_DATE_FORMATS } from './date-formats';
@@ -7,7 +7,16 @@ import { NX_DATE_LOCALE } from './date-token';
 import { NxNativeDateAdapter } from './native-date-adapter';
 import { NX_NATIVE_DATE_FORMATS } from './native-date-formats';
 
-export const NX_DATE_LOCALE_PROVIDER = { provide: NX_DATE_LOCALE, useExisting: LOCALE_ID };
+export const NX_DATE_LOCALE_PROVIDER = {
+    provide: NX_DATE_LOCALE,
+    useFactory: (localeId: any, nxDateLocale: any) => {
+        if (nxDateLocale !== null) {
+            return nxDateLocale;
+        }
+        return localeId;
+    },
+    deps: [LOCALE_ID, [new SkipSelf(), new Optional(), NX_DATE_LOCALE]],
+};
 
 export * from './date-adapter';
 export * from './date-formats';
