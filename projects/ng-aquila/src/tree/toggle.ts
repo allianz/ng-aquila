15caf2136c2fef5f6c1e98018d5a4258ece1aeac
viviz.thapunyaphat@allianz.com <viviz.thapunyaphat@allianz.com>
2024-01-25 19:28:28 +0700

    fix(tree): use inputs alias to mitigate CDK change (#1113)

--- projects/ng-aquila/src/tree/toggle.ts
+++ projects/ng-aquila/src/tree/toggle.ts
@@ -1,19 +1,12 @@
-import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
 import { CdkTreeNodeToggle } from '@angular/cdk/tree';
-import { Directive, Input } from '@angular/core';
+import { Directive } from '@angular/core';
 
 /**
  * Wrapper for the CdkTree's toggle with custom design styles.
  */
 @Directive({
     selector: '[nxTreeNodeToggle]',
+    inputs: ['recursive: nxTreeNodeToggleRecursive'],
     providers: [{ provide: CdkTreeNodeToggle, useExisting: NxTreeNodeToggleDirective }],
 })
-export class NxTreeNodeToggleDirective<T> extends CdkTreeNodeToggle<T> {
-    @Input('nxTreeNodeToggleRecursive') set recursive(value: BooleanInput) {
-        this._recursive = coerceBooleanProperty(value);
-    }
-    get recursive(): boolean {
-        return this._recursive;
-    }
-}
+export class NxTreeNodeToggleDirective<T> extends CdkTreeNodeToggle<T> {}
