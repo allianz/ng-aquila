f4ac95d65740ff0100d08675fcc2d94aa643028c
malin.klingsell@allianz.de <malin.klingsell@allianz.com>
2024-04-11 09:13:07 +0200

    fix(table): remove clipcell and width calculation (#1162)

--- projects/ng-aquila/src/table/resizing/table-cell-clip.directive.ts
+++ projects/ng-aquila/src/table/resizing/table-cell-clip.directive.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive } from '@angular/core';
 
 @Directive({
     selector: 'table[nxTableCellClip]',
@@ -6,28 +6,4 @@ import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
         '[style.table-layout]': '"fixed"',
     },
 })
-export class NxTableCellClipDirective implements AfterViewInit {
-    constructor(
-        private elementRef: ElementRef,
-        private renderer: Renderer2,
-    ) {}
-
-    clipCell(cell: any) {
-        this.renderer.setStyle(cell, 'white-space', 'nowrap');
-        this.renderer.setStyle(cell, 'overflow', 'hidden');
-        this.renderer.setStyle(cell, 'text-overflow', 'ellipsis');
-    }
-
-    ngAfterViewInit() {
-        const tds = this.elementRef.nativeElement.querySelectorAll('td');
-        tds.forEach((cell: any) => {
-            this.clipCell(cell);
-        });
-        const ths = this.elementRef.nativeElement.querySelectorAll('th');
-        ths.forEach((cell: any) => {
-            this.clipCell(cell);
-            const width = cell?.getBoundingClientRect().width;
-            this.renderer.setStyle(cell, 'width', `${width}px`);
-        });
-    }
-}
+export class NxTableCellClipDirective {}
