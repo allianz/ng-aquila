e1028ba81a071bab504c2547adab2740c61cdfeb
Phil147 <Phil147@users.noreply.github.com>
2024-06-26 16:25:52 +0000

    Deploying to gh-pages from  @ 25c8fffbb5c3063bfe1914ed2e72987af0bb0572 🚀

---
+++ docs-content/examples-source/timefield/timefield-options-interval/timefield-options-interval-example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Timefield different options
+ */
+@Component({
+    selector: 'nx-timefield-options-interval-example',
+    templateUrl: './timefield-options-interval-example.html',
+    styleUrls: ['./timefield-options-interval-example.css'],
+})
+export class TimefieldOptionsIntervalExampleComponent {}
