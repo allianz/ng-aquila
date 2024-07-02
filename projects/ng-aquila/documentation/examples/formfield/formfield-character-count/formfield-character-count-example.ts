b3065a43ddd78dfd45574ae8f2d9a7f6f1fdb875
viviz.thapunyaphat@allianz.com <viviz.thapunyaphat@allianz.com>
2024-07-02 16:42:56 +0700

    docs(formfield): correct count remaining example (#1236)
    * docs(formfield): correct counting remaining in examples
    
    * docs(formfield): correct counting remaining in examples

--- projects/ng-aquila/documentation/examples/formfield/formfield-character-count/formfield-character-count-example.ts
+++ projects/ng-aquila/documentation/examples/formfield/formfield-character-count/formfield-character-count-example.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NxInputDirective } from '@aposin/ng-aquila/input';
 
 /**
  * @title Character count example
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
     templateUrl: './formfield-character-count-example.html',
     styleUrls: ['./formfield-character-count-example.css'],
 })
-export class FormfieldCharacterCountExampleComponent {}
+export class FormfieldCharacterCountExampleComponent {
+    @ViewChild('inputToCount', { read: NxInputDirective, static: true })
+    input!: NxInputDirective;
+
+    count = 0;
+
+    onInput() {
+        this.count = this.input.value.length;
+    }
+}
