6b4abb7db65490c3c81204733ddd51ef2e6b2abd
viviz.thapunyaphat@allianz.com <viviz.thapunyaphat@allianz.com>
2025-01-14 22:50:22 +0700

    fix(dropdown): add group role to make screenreader read correctly (#1397)

--- projects/ng-aquila/src/dropdown/group/dropdown-group.ts
+++ projects/ng-aquila/src/dropdown/group/dropdown-group.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { randomString } from '@aposin/ng-aquila/utils';
 
 @Component({
     selector: 'nx-dropdown-group',
@@ -6,8 +7,16 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrls: ['dropdown-group.scss'],
     standalone: true,
+    host: {
+        role: 'group',
+        '[attr.aria-labelledby]': 'id',
+    },
 })
 export class NxDropdownGroupComponent {
+    private static nextId = 0;
+
+    id = `nx-dropdown-group-${randomString()}-${NxDropdownGroupComponent.nextId++}`;
+
     /** Label displayed in the group. */
     @Input() label!: string;
 }
