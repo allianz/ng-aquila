a2f96c67ec68ca023f75d939e7a4d8811a83b794
Philipp Fahrenschon <philipp.fahrenschon@satellytes.com>
2025-07-14 16:59:52 +0200

    chore: fix issue template

--- projects/ng-aquila/documentation/assets/stackblitz-issue-template/src/main.ts
+++ projects/ng-aquila/documentation/assets/stackblitz-issue-template/src/main.ts
@@ -1,7 +1,7 @@
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptorsFromDi, withJsonpSupport } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { RouterModule, provideRouter } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { NxDocumentationIconModule } from '@allianz/ng-aquila/documentation-icons';
 import { NxIsoDateModule } from '@allianz/ng-aquila/iso-date-adapter';
 
