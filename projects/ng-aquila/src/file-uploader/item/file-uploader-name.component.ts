2998c844cd4b6974c6e1cf8f9e5f7adb34a82d9b
felix.hutterer@allianz.de <felix.hutterer@allianz.de>
2025-09-29 17:59:24 +0200

    fix(file-uploader): long file names can wrap to new line (#1603)

--- projects/ng-aquila/src/file-uploader/item/file-uploader-name.component.ts
+++ projects/ng-aquila/src/file-uploader/item/file-uploader-name.component.ts
@@ -16,7 +16,7 @@ import { getFileExtension } from '../file-uploader.validations';
       }
       <nx-icon name="file" class="extension-icon"></nx-icon>
     </span>
-    <span class="nx-margin-left-2xs">{{ name }}</span>
+    <span class="file-name nx-margin-left-2xs">{{ name }}</span>
   `,
   imports: [NxIconModule],
 })
