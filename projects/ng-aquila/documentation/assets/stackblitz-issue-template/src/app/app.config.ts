2f5ff5f51ea8a0103879001c18c6031a7b7822bb
alexander-schilpp <alexander.schilpp@allianz.com>
2025-12-15 18:01:03 +0700

    chore(ide): reduce indent size in editorconfig from 4 to 2 & apply consistently (#1703)
    * chore(ide): reduce indent size in editorconfig from 4 to 2
    
    * chore(indentation): apply correct indentation setting to project

--- projects/ng-aquila/documentation/assets/stackblitz-issue-template/src/app/app.config.ts
+++ projects/ng-aquila/documentation/assets/stackblitz-issue-template/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import {
-    ApplicationConfig,
-    provideBrowserGlobalErrorListeners,
-    provideZoneChangeDetection,
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
 } from "@angular/core";
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideBrowserGlobalErrorListeners(),
-        provideZoneChangeDetection({ eventCoalescing: true }),
-    ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+  ],
 };
--- projects/ng-aquila/documentation/assets/stackblitz-issue-template/src/app/app.ts
+++ projects/ng-aquila/documentation/assets/stackblitz-issue-template/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from "@angular/core";
 import { NxButtonComponent } from "@allianz/ng-aquila/button";
 
 @Component({
-    selector: "app-root",
-    imports: [NxButtonComponent],
-    templateUrl: "./app.html",
-    styleUrl: "./app.scss",
+  selector: "app-root",
+  imports: [NxButtonComponent],
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
 })
 export class App {
-    protected readonly title = signal("test-project");
+  protected readonly title = signal("test-project");
 }
