ced48284c362d8507547f13a05207a06e29f36ce
Philipp Fahrenschon <extern.fahrenschon_philipp1@allianz.de>
2026-06-23 11:20:05 +0200

    feat(button): add icon button padding tokens for A1 (#1911)

--- projects/ng-aquila/documentation/examples/button/button-icon/button-icon-example.ts
+++ projects/ng-aquila/documentation/examples/button/button-icon/button-icon-example.ts
@@ -1,6 +1,12 @@
-import { NxIconButtonComponent } from '@allianz/ng-aquila/button';
+import { NxButtonSize, NxIconButtonComponent } from '@allianz/ng-aquila/button';
 import { NxIconComponent } from '@allianz/ng-aquila/icon';
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { NxFormfieldComponent } from '@allianz/ng-aquila/formfield';
+import {
+  NxDropdownComponent,
+  NxDropdownItemComponent,
+} from '@allianz/ng-aquila/dropdown';
+import { FormsModule } from '@angular/forms';
 
 /**
  * @title Icon Button Example
@@ -9,6 +15,15 @@ import { Component } from '@angular/core';
   selector: 'button-icon-example',
   templateUrl: './button-icon-example.html',
   styleUrls: ['./button-icon-example.css'],
-  imports: [NxIconButtonComponent, NxIconComponent],
+  imports: [
+    NxIconButtonComponent,
+    NxIconComponent,
+    NxFormfieldComponent,
+    NxDropdownComponent,
+    NxDropdownItemComponent,
+    FormsModule,
+  ],
 })
-export class ButtonIconExampleComponent {}
+export class ButtonIconExampleComponent {
+  size = model<NxButtonSize>('medium');
+}
--- projects/ng-aquila/src/icon/default-icons.ts
+++ projects/ng-aquila/src/icon/default-icons.ts
@@ -33,6 +33,7 @@ export class NxDefaultIcons {
   'popout'?: string;
   'arrow-up'?: string;
   'arrow-down'?: string;
+  'double-chevron-small'?: string;
 }
 
 export const DEFAULT_ICONS: { [k: string]: string } = {
@@ -101,6 +102,8 @@ export const DEFAULT_ICONS: { [k: string]: string } = {
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M424.5 355.1C449 329.2 464 294.4 464 256C464 176.5 399.5 112 320 112C240.5 112 176 176.5 176 256C176 294.4 191 329.2 215.5 355.1C236.8 377.5 260.4 409.1 268.8 448L371.2 448C379.6 409 403.2 377.5 424.5 355.1zM459.3 388.1C435.7 413 416 443.4 416 477.7L416 496C416 540.2 380.2 576 336 576L304 576C259.8 576 224 540.2 224 496L224 477.7C224 443.4 204.3 413 180.7 388.1C148 353.7 128 307.2 128 256C128 150 214 64 320 64C426 64 512 150 512 256C512 307.2 492 353.7 459.3 388.1zM272 248C272 261.3 261.3 272 248 272C234.7 272 224 261.3 224 248C224 199.4 263.4 160 312 160C325.3 160 336 170.7 336 184C336 197.3 325.3 208 312 208C289.9 208 272 225.9 272 248z"/></svg>',
   'ai-suggestion':
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M420.9 448C428.2 425.7 442.8 405.5 459.3 388.1C492 353.7 512 307.2 512 256C512 150 426 64 320 64C214 64 128 150 128 256C128 307.2 148 353.7 180.7 388.1C197.2 405.5 211.9 425.7 219.1 448L420.8 448zM416 496L224 496L224 512C224 556.2 259.8 592 304 592L336 592C380.2 592 416 556.2 416 512L416 496zM312 176C272.2 176 240 208.2 240 248C240 261.3 229.3 272 216 272C202.7 272 192 261.3 192 248C192 181.7 245.7 128 312 128C325.3 128 336 138.7 336 152C336 165.3 325.3 176 312 176z"/></svg>',
+  'double-chevron-small':
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M342.6 105.4C330.1 92.9 309.8 92.9 297.3 105.4L137.3 265.4C124.8 277.9 124.8 298.2 137.3 310.7C149.8 323.2 170.1 323.2 182.6 310.7L320 173.3L457.4 310.6C469.9 323.1 490.2 323.1 502.7 310.6C515.2 298.1 515.2 277.8 502.7 265.3L342.7 105.3zM502.6 457.4L342.6 297.4C330.1 284.9 309.8 284.9 297.3 297.4L137.3 457.4C124.8 469.9 124.8 490.2 137.3 502.7C149.8 515.2 170.1 515.2 182.6 502.7L320 365.3L457.4 502.6C469.9 515.1 490.2 515.1 502.7 502.6C515.2 490.1 515.2 469.8 502.7 457.3z"/></svg>',
 };
 
 DEFAULT_ICONS['exclamation-circle-warning'] = DEFAULT_ICONS['exclamation-circle'];
